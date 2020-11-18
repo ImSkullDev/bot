b13f5daf28a81cbca5002a531b3425c7e018196b
Jacob <16949253+PassTheMayo@users.noreply.github.com>
2020-11-17 22:53:43 -0600

    Fix bug with eval

--- src/Util/formatArbitrary.js
+++ src/Util/formatArbitrary.js
@@ -2,8 +2,8 @@ const config = require('../config');
 
 module.exports = (data) => {
 	data = data.replace(new RegExp(config.bot.token, 'g'), '-- SENSITIVE INFORMATION --');
-	data = data.replace(new RegExp(config.mongo.password, 'g'), '-- SENSITIVE INFORMATION --');
-	data = data.replace(new RegExp(config.mongo.host, 'g'), '-- SENSITIVE INFORMATION --');
+	// data = data.replace(new RegExp(config.mongo.password, 'g'), '-- SENSITIVE INFORMATION --');
+	// data = data.replace(new RegExp(config.mongo.host, 'g'), '-- SENSITIVE INFORMATION --');
 	data = data.replace(new RegExp(config.redis.password, 'g'), '-- SENSITIVE INFORMATION --');
 
 	return data;
